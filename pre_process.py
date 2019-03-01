147364ef8c01a6c05153e7eb1cb8e6f620011c61
Yang Liu <liuyang12@focusmedia.cn>
2019-03-01 14:12:33 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -18,17 +18,32 @@ def get_data(mode):
         with open(trn_path, 'r', encoding='utf-8') as file:
             trn = file.readline()
         trn = trn.strip().replace(' ', '')
+        for token in trn:
+            build_vocab(token)
         samples.append({'wave': w, 'trn': trn})
     return samples
 
 
-if __name__ == "__main__":
-    data = dict()
+def build_vocab(token):
+    if not token in VOCAB:
+        next_index = len(VOCAB)
+        VOCAB[token] = next_index
+        IVOCAB[next_index] = token
 
+
+if __name__ == "__main__":
+    VOCAB = {'<PAD>': 0, '<SOS>': 1, '<EOS>': 2}
+    IVOCAB = {0: '<PAD>', 1: '<SOS>', 2: '<EOS>'}
+
+    data = dict()
+    data['VOCAB'] = VOCAB
+    data['IVOCAB'] = IVOCAB
     data['train'] = get_data('train')
-    print('num_train: ' + str(len(data['train'])))
     data['test'] = get_data('test')
+
+    print('num_train: ' + str(len(data['train'])))
     print('num_test: ' + str(len(data['test'])))
+    print('vocab_size: ' + str(len(VOCAB)))
 
     with open(pickle_file, 'wb') as file:
         pickle.dump(data, file)

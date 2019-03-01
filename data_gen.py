a01b2ec0cdcb70d891f8d35b61bf298aa0e6baa6
Yang Liu <liuyang12@focusmedia.cn>
2019-03-01 17:55:09 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -20,13 +20,13 @@ def pad_collate(batch):
     for i, elem in enumerate(batch):
         f, trn = elem
         input_dim = f.shape[1]
-        print('f.shape: ' + str(f.shape))
+        # print('f.shape: ' + str(f.shape))
         feature = np.zeros((max_input_len, input_dim), dtype=np.float)
         feature[:f.shape[0], :f.shape[1]] = f
         trn = np.pad(trn, (0, max_target_len - len(trn)), 'constant', constant_values=0)
         batch[i] = (feature, trn)
-        print('feature.shape: ' + str(feature.shape))
-        print('trn.shape: ' + str(trn.shape))
+        # print('feature.shape: ' + str(feature.shape))
+        # print('trn.shape: ' + str(trn.shape))
     return default_collate(batch)
 
 

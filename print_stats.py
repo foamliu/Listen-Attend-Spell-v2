f3de8432254729363385293a4a55a3307f5adfbf
Yang Liu <liuyang12@focusmedia.cn>
2019-03-22 18:59:12 +0800

    udpate

---
+++ print_stats.py
@@ -0,0 +1,12 @@
+from config import *
+
+if __name__ == "__main__":
+    for mode in ['train', 'dev', 'test']:
+        folder = os.path.join(wav_folder, mode)
+        dirs = [os.path.join(folder, d) for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
+        num_files = 0
+        for dir in dirs:
+            files = [os.path.join(dir, f) for f in os.listdir(dir) if f.endswith('.wav')]
+            num_files += len(files)
+
+        print('mode: {}, num_files: {}'.format(mode, num_files))

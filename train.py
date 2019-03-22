73820d62a910c15e498491d9730807ba6fb0f3a1
Yang Liu <liuyang12@focusmedia.cn>
2019-03-22 19:38:25 +0800

    update

--- train.py
+++ train.py
@@ -29,8 +29,7 @@ def train_net(args):
             optimizer = torch.optim.SGD([{'params': encoder.parameters()}, {'params': decoder.parameters()}],
                                         lr=args.lr, momentum=args.mom, weight_decay=args.weight_decay)
         else:
-            optimizer = torch.optim.Adam([{'params': encoder.parameters()}, {'params': decoder.parameters()}],
-                                         lr=args.lr, weight_decay=args.weight_decay)
+            optimizer = torch.optim.Adam([{'params': encoder.parameters()}, {'params': decoder.parameters()}])
 
     else:
         checkpoint = torch.load(checkpoint)
@@ -54,11 +53,11 @@ def train_net(args):
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, collate_fn=pad_collate,
                                                shuffle=False, drop_last=True)
 
-    scheduler = StepLR(optimizer, step_size=args.lr_step, gamma=0.5)
+    # scheduler = StepLR(optimizer, step_size=args.lr_step, gamma=0.5)
 
     # Epochs
     for epoch in range(start_epoch, args.end_epoch):
-        scheduler.step()
+        # scheduler.step()
 
         # One epoch's training
         train_loss = train(train_loader=train_loader,
